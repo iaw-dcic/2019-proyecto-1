6e7dc0873e2183e7d08c352ec2cc3322ff361061
Pablo Guillermo <pgcb123@gmail.com>
2019-04-10 16:12:03 -0300

    SQL catalogo

---
+++ Base de Datos/CatalogoJuegos.sql
@@ -0,0 +1,31 @@
+#Base de Datos para la Página web del Proyecto 1 de IAW
+
+#Creo la base de datos propiamente dicha
+
+CREATE DATABASE catalogojuegos;
+
+
+#Selecciono la base de datos a usar
+USE catalogojuegos;
+
+#-------------------------------------------------------------------------
+
+#Creo las tablas para las Entidades
+
+CREATE TABLE Usuario(
+
+
+)Engine = InnoDB;
+
+
+CREATE TABLE Juego(
+
+
+)Engine = InnoDB;
+
+
+CREATE TABLE ListaJuegos(
+
+
+
+)Engine = InnoDB;
