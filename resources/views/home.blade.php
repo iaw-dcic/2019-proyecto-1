ab64a0e9211cbdf5b22f9ed8672b0a85e21ce41c
Matias <matimassetti@gmail.com>
2019-04-10 21:45:44 -0300

    edit text in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,16 +73,16 @@
         <div class="hero-slider owl-carousel">
             <div class="hero-item set-bg d-flex align-items-center justify-content-center text-center" data-setbg="img/slider-bg-1.jpg">
                 <div class="container">
-                    <h2>GoodGame</h2>
-                    <p>Todos tus juegos en un solo lugar!.</p>
+                    <h2>GoodGame!</h2>
+                    <p>Todos tus juegos en un solo lugar!</p>
                     <a href="./about-us" class="site-btn">Leer más<img src="img/icons/double-arrow.png" alt="#" /></a>
                 </div>
             </div>
             <div class="hero-item set-bg d-flex align-items-center justify-content-center text-center" data-setbg="img/slider-bg-2.jpg">
                 <div class="container">
-                    <h2>Game on!</h2>
-                    <p>Fusce erat dui, venenatis et erat in, vulputate dignissim lacus. Donec vitae tempus dolor,<br>sit amet elementum lorem. Ut cursus tempor turpis.</p>
-                    <a href="./about-us" class="site-btn">Read More <img src="img/icons/double-arrow.png" alt="#" /></a>
+                    <h2>texto</h2>
+                    <p>otro texto</p>
+                    <a href="./about-us" class="site-btn">Leer más<img src="img/icons/double-arrow.png" alt="#" /></a>
                 </div>
             </div>
         </div>
