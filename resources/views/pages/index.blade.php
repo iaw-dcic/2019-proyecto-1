8ade3de963a7e736f59d5d9f0f895341c0031f13
Sebastian Salaberria <s_salaberria@hotmail.com>
2019-04-14 20:26:54 -0300

    Placeholder page eliminada. Agregado PagesController.

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Proyecto IAW-2019</title>
+    </head>
+    <body>
+        <h1>Proyecto 1: PHP - Laravel - MySQL</h1>
+    </body>
+</html>
