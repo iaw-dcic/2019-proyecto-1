2a3904e11759ae6bb4da601942a4d83d3eb4ee77
Pablo Guillermo <pgcb123@gmail.com>
2019-04-15 08:49:41 -0300

    creacion esquemas de tablas
    más esquemas para los datos, ver tema listas e items asociados a ellos

---
+++ database/migrations/2019_04_16_create_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_info', function (Blueprint $table) {
+            $table->string('name')->index()->nullable($value=false)->unique();
+            $table->string('genre');
+            $table->string('company');
+            $table->date('release_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_info');
+    }
+}
---
+++ database/migrations/2019_04_16_create_lists_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('list_info', function (Blueprint $table) {
+            $table->string('name')->index()->nullable($value=false)->unique();
+            $table->increments('id')->autoincrement()->unique();
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_info');
+    }
+}
