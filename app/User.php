b8cc51b7c49c7a9290153f439995d03d13127369
JoseOchoaUns <joseochoa.uns@gmail.com>
2019-04-15 12:49:27 -0300

    Modelo de profile, Controlador creado

--- app/User.php
+++ app/User.php
@@ -10,6 +10,9 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function profile(){
+        return $this->hasOne(Profile::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
