88c85a0be4192ee90abbb811aa4c5e5a5f456ed5
Tomas Miguel <tomasmiguelf94@gmail.com>
2019-04-19 15:42:02 -0300

    BD y modelos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Hola Che!
                 </div>
 
                 <div class="links">
