90dd28f4ea0fb799ebf698c02786b42d2b6c0e28
Manuela <manuela.dv.fernandez@gmail.com>
2019-04-23 09:24:31 -0300

    cambios en la fuentes de letras

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,300,400,800|Roboto:100" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
