a108ade930e0f519614531e606918d90e9ffef1b
joaquinmontero <31543510+joaquinmontero@users.noreply.github.com>
2019-04-27 03:01:18 -0300

    added bd and controllers
    added all databases migrations and all controllers

--- database/migrations/2019_04_27_044357_create_lista_table.php
+++ database/migrations/2019_04_27_044357_create_lista_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateListaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lista', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email',100);
-            $table->string('biografia');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('userid');
+            $table->foreign('userid')->references('id')->on('users');
+            $table->string('nombre');
+            $table->boolean('visible');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('lista');
     }
 }
