067e7f246c19859c6d50ae6cf55e645bb9f0ee75
Matias <matimassetti@gmail.com>
2019-04-27 12:53:51 -0300

    working with social buttons in login

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -55,6 +55,7 @@
   <!--Multiple select forms Bootstrap-->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
 
 
 </head>
