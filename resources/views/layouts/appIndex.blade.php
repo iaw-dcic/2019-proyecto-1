118daac9e1fe566f898fc23b897ab049379fb015
JoseOchoaUns <joseochoa.uns@gmail.com>
2019-04-28 17:08:10 -0300

    Pequeño tweak para el audit de performance

--- resources/views/layouts/appIndex.blade.php
+++ resources/views/layouts/appIndex.blade.php
@@ -9,7 +9,7 @@
   <meta name="author" content="">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- Scripts -->
-  <script src="{{ asset('js/app.js') }}"></script>
+  <script defer src="{{ asset('js/app.js') }}"></script>
     <!-- Bootstrap core JavaScript -->
 
   <title>Bienes web</title>
@@ -18,7 +18,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" async href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 
 </head>
