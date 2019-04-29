01abbe2a03d23daea837179729bd5efbc690278b
francoschroeder <yofrancoschroeder@yahoo.com.ar>
2019-04-29 13:43:17 -0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,21 +46,29 @@ class LoginController extends Controller
 
     public function handleProviderFacebookCallback() {
         $auth_user = Socialite::driver('facebook')->user();
+
+        $user = User::where('email', $auth_user->email);
+
+        if ($user==null) {
+            $user = new User();
+            $user->name = $auth_user->name;
+            $user->email = $auth_user->email;
+            $user->token = $auth_user->token;
+            $user->save();
+        } else
+            $user->token = $auth_user->token;
         
-        $user = User::updateOrCreate(
+        /*$user = User::updateOrCreate(
             [
                 'email' => $auth_user->email
             ],
             [
                 'token' => $auth_user->token,
             ]
-        );
+        );*/
 
 
-            dd($auth_user);
-
-        if ($user->name == null)
-            $user->name = $auth_user->name;
+        dd($auth_user);
 
         Auth::login($user, true);
 
