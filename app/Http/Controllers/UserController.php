7869eb01b614da46adef4930541e65dcd6a1ff7d
francoschroeder <yofrancoschroeder@yahoo.com.ar>
2019-04-29 12:41:14 -0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,7 @@ class UserController extends Controller
     }
 
     public function updatePassword() {
-        $user = $auth()->user();
+        $user = auth()->user();
 
         if ($user == null)
             return view('auth.login');
