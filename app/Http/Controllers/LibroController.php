5b519843079ab35ddf1ec41541c6178035549cd9
JoseOchoaUns <joseochoa.uns@gmail.com>
2019-04-29 19:27:46 -0300

    validacion edicion de libro

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -38,8 +38,8 @@ class LibroController extends Controller
             'Genero' => 'required',
             'Titulo' => 'required',
             'Autor' => 'required',
-            'FechaPublicacion' => 'required'
         ]);
+
         $libro = Libro::findOrFail($request->id_libro);
         $libro -> Titulo = $request->titulo;
         $libro -> Autor = $request->autor;
