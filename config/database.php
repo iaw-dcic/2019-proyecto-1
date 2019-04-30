3658f8c0fd6d51f92dfeb154db03740df4ec98ac
Lucas Monzón <monzon.lucas4@gmail.com>
2019-04-30 17:58:25 -0300

    Conexión a heroku

--- config/database.php
+++ config/database.php
@@ -44,11 +44,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-gcp-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'gcp_43b06802eb66400c46a5'),
+            'username' => env('DB_USERNAME', 'b0e2dcaf394811'),
+            'password' => env('DB_PASSWORD', 'cd4d01eb'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
