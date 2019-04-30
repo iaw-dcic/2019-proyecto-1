538d1285dfd5b6d0514ab96ed9486918db7a8111
gaeprivitello <gaeprivi>
2019-04-30 19:44:39 -0300

    entrega

--- app/User.php
+++ app/User.php
@@ -14,6 +14,9 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var array
+     *
+     *
+     * 
      */
     protected $fillable = [
         'name', 'email', 'password',
