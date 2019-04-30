fc66565c38f6be4f7595377581f4330ce44cb1bb
Matias <matimassetti@gmail.com>
2019-04-29 22:15:22 -0300

    changes in about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,10 +5,10 @@
 <!-- Page top section -->
 <section class="page-top-section set-bg" data-setbg="img/page-top-bg/4.jpg">
     <div class="page-info">
-        <h2>Informacion</h2>
+        <h2>Preguntas frecuentes</h2>
         <div class="site-breadcrumb">
             <a href="/">Inicio</a> /
-            <span>Informacion</span>
+            <span>Preguntas frecuentes</span>
         </div>
     </div>
 </section>
@@ -18,46 +18,129 @@
 <!-- Contact page -->
 <section class="contact-page">
     <div class="container">
-        <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14376.077865872314!2d-73.879277264103!3d40.757667781624285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1546528920522"
-                style="border:0" allowfullscreen></iframe></div>
-        <div class="row">
-            <div class="col-lg-7 order-2 order-lg-1">
-                <form class="contact-form">
-                    <input type="text" placeholder="Your name">
-                    <input type="text" placeholder="Your e-mail">
-                    <input type="text" placeholder="Subject">
-                    <textarea placeholder="Message"></textarea>
-                    <button class="site-btn">Send message<img src="img/icons/double-arrow.png" alt="#" /></button>
-                </form>
-            </div>
-            <div class="col-lg-5 order-1 order-lg-2 contact-text text-white">
-                <h3>Howdy! Say hello</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....</p>
-                <div class="cont-info">
-                    <div class="ci-icon"><img src="img/icons/location.png" alt=""></div>
-                    <div class="ci-text">Main Str, no 23, New York</div>
+        <div class="panel-group" id="accordion">
+            <div class="faqHeader"><i class="fa fa-question-circle-o" aria-hidden="true"></i> General </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">¿Qué puedo hacer <strong>en Good Game? <strong></a>
+                    </h4>
                 </div>
-                <div class="cont-info">
-                    <div class="ci-icon"><img src="img/icons/phone.png" alt=""></div>
-                    <div class="ci-text">+546 990221 123</div>
-                </div>
-                <div class="cont-info">
-                    <div class="ci-icon"><img src="img/icons/mail.png" alt=""></div>
-                    <div class="ci-text">hosting@contact.com</div>
+                <div id="collapseOne" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        En Good Game podes administrar tu inventario de juegos, creando listas y compartiéndolas con otros usuarios!
+                    </div>
                 </div>
             </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">¿Es necesario registrarse?</a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse in" style="color:snow">
+                    <div class="panel-body">
+                         Podes ver las listas públicas de otros usuarios y sus perfiles aun sin estar registrado. Para crear tus propias listas el registro es necesario.                 
+                    </div>
+                </div>
+            </div>
+            
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">¿Por qué el sitio se llama Good Game?</a>
+                    </h4>
+                </div>
+                <div id="collapseThree" class="panel-collapse collapse">
+                    <div class="panel-body">
+                       GG o good game es una expresión dentro del mundo gamer para reconocer una buena partida al adversario. 
+                    </div>
+                </div>
+            </div>
+    
+            <div class="faqHeader"><i class="fa fa-laptop" aria-hidden="true"></i> Funcionamiento</div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">Crear lista</a>
+                    </h4>
+                </div>
+                <div id="collapseFour" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Podes crear una lista sólo si iniciaste sesión. Para crearla ingresa a la opción en el menu superior. Ponele un nombre, elegí si queres que sea pública o privada y listo!.
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">Agregar juego</a>
+                    </h4>
+                </div>
+                <div id="collapseFive" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Podes agregar un juego a una de tus listas seleccionándola en el menú supeior "Listas" y agregándola desde ahí. También podes seleccionar la opción "Agregar juego" y seleccionando a que listas lo querés agregar
+                       
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseSix">¿Puedo agregar un juego a más de una lista?</a>
+                    </h4>
+                </div>
+                <div id="collapseSix" class="panel-collapse collapse">
+                    <div class="panel-body">
+                            Sí, podes agregarlo a las listas que quieras!
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven">¿Qué más puedo hacer?</a>
+                    </h4>
+                </div>
+                <div id="collapseSeven" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Podes editar tus listas y juegos, editar tu perfil o buscar listas de otros usuarios.    
+                    </div>
+                </div>
+            </div>
+            
+            <div class="faqHeader"><i class="fa fa-copyright" aria-hidden="true"></i> Copyright</div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseEight"> Template </a>
+                    </h4>
+                </div>
+                <div id="collapseEight" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        El template general del sitio pertenece a <a href="https://themewagon.com/author/kimlabs/" >Colorlib </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="faqHeader"><i class="fa fa-envelope-o" aria-hidden="true"></i></i> Contacto </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" style="color:whitesmoke" data-toggle="collapse" data-parent="#accordion" href="#collapseNine"> Comunicarse por mail </a>
+                    </h4>
+                </div>
+                <div id="collapseNine" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Envia tu consulta o sugerencia <a href="mailto:matiasmassetti@gmail.com" target="_top">acá</a>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     </div>
 </section>
 <!-- Contact page end-->
 
-<!-- Newsletter section -->
-<section class="newsletter-section">
-    <div class="container">
-        <h2>Algun texto o funcionalidad o ver propiedades css</h2>
-    </div>
-</section>
-<!-- Newsletter section end -->
 @endsection
