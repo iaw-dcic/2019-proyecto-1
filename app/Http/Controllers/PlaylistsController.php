3cf228d3eab207213c15d60e1e86005167d5ada6
Rodrigo Díaz <rodrigomdiaz@live.com>
2019-04-30 15:39:29 -0300

    arreglado guest puede ver playlists publicas

--- app/Http/Controllers/PlaylistsController.php
+++ app/Http/Controllers/PlaylistsController.php
@@ -10,7 +10,7 @@ class PlaylistsController extends Controller
 {
     public function __construct(){
 
-        $this->middleware('auth')->except(['show']);
+        $this->middleware('auth')->except(['index','show']);
 
     }
 
