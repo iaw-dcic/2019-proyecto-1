6a8ed212a05f65026273e9952166daa8e5a47aab
gaston <reyesgaston_95@yahoo.com.ar>
2019-04-30 20:36:56 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -44,8 +44,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', ''),
-            'port' => env('DB_PORT', ''),
+            'host' => env('DB_HOST', 'us-cdbr-gcp-east-01.cleardb.net'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', ''),
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
