d151144b0e6d5a281d0049175cb2a5986f4767ba
agussgarciaa <agussgarciaa9@gmail.com>
2019-04-30 18:18:27 -0300

    Se arreglo el readme.

--- resources/views/readme.blade.php
+++ resources/views/readme.blade.php
@@ -4,10 +4,54 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Readme</div>
+                <div class="card-header">
+                <h2>Readme</h2><br>
+                    Author: Agustin Emanuel Garcia
+                </div>
+                
+                <div class="card-body">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <td>Page</td>
+                                <td>Performance</td>
+                                <td>Accessibility</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <tr>
+                                    <td>Welcome</td>
+                                    <td>99</td>
+                                    <td>71</td>
+                                </tr>
+                                <tr>
+                                    <td>Explore</td>
+                                    <td>85</td>
+                                    <td>85</td>
+                                </tr>
+                                <tr>
+                                    <td>Readme</td>
+                                    <td>85</td>
+                                    <td>84</td>
+                                </tr>
+                                <tr>
+                                    <td>Login</td>
+                                    <td>80</td>
+                                    <td>87</td>
+                                </tr>
+                                <tr>
+                                    <td>Register</td>
+                                    <td>87</td>
+                                    <td>85</td>
+                                </tr>
+                        
+                        </tbody>
+                    </table>
+            </div>
 
                 <div class="card-body">
                    Author: Agustin Emanuel Garcia
+
                    
                 </div>
             </div>
