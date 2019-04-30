3604def5ee22ab44001d3d32369015953414ff66
Matias <matimassetti@gmail.com>
2019-04-30 14:02:55 -0300

    to test in heroku

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/searchlisting', 'PagesController@searchListing');
 Route::resource('games','GamesController');
 
 //Auth
-Auth::routes();
+Auth::routes(['verify' => true]);
 Route::get('login/{service}', 'Auth\LoginController@redirectToProvider');
 Route::get('login/{service}/callback', 'Auth\LoginController@handleProviderCallback');
 
