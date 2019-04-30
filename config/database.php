9e24ccdf798cc7fed862e1701ca00ea81e566581
MartinHredil <hredilmartin@gmail.com>
2019-04-30 00:56:12 -0300

    Configuracion DB Web

--- config/database.php
+++ config/database.php
@@ -44,11 +44,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-gcp-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'gcp_447c95ff38ca2080dc15'),
+            'username' => env('DB_USERNAME', 'bf4e4a1bbd6ac2'),
+            'password' => env('DB_PASSWORD', '011fc052'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
