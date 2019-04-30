c3063e4267d79a1f027be3832a754948bc4c7793
Lucas Monzón <monzon.lucas4@gmail.com>
2019-04-30 18:03:42 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(150);
+        Schema::defaultStringLength(191);
     }
 }
