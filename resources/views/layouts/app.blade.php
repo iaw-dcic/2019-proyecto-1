ee7a8d7b72acc01cbc5084c12847338313bbdb54
agus-str <agus.stragua@gmail.com>
2019-05-01 13:11:40 -0300

    ...

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,24 +28,5 @@
             </div>
         </main>
     </div>
-     @if(count($errors) > 0)
-    @foreach($errors->all() as $error)
-    <div class="alert alert-danger">
-        {{$error}}
-    </div>
-    @endforeach
-@endif
-
- @if(session('success'))
-    <div class="alert alert-succes">
-        {{session('success')}}
-    </div>
-@endif
-
- @if(session('error'))
-    <div class="alert alert-danger">
-        {{session('error')}}
-    </div>
-@endif
 </body>
 </html>
