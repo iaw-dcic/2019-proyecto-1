ddb2e253d049c209421a0e44e0be2718db3d40c0
gaston <reyesgaston_95@yahoo.com.ar>
2019-04-30 22:35:11 -0300

    Update ColeccionController.php

--- app/Http/Controllers/ColeccionController.php
+++ app/Http/Controllers/ColeccionController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Request\ColeccionFormRequest;
 
+//use App\Categoria;
 use App\Categoria;
 
 class ColeccionController extends Controller
