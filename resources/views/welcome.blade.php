3cbe47dc2ae08a115db01c5641b48c35ce63a651
Constanza Giorgetti <coti.giorgetti@hotmail.com>
2019-05-01 20:09:44 -0300

    Se quito el script welcome.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,6 +18,6 @@
         </section>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>   
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="{{asset('js/welcome.js')}}"></script>
+
     </body>
 </html>
