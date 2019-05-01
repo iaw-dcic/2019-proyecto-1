6329a0819518042ece8c6aaa8231f1bd395cf809
Constanza Giorgetti <coti.giorgetti@hotmail.com>
2019-05-01 19:40:20 -0300

    Se corrigio un script para que sea seguro

--- public/js/user.js
+++ public/js/user.js
@@ -2,7 +2,7 @@ $(function () {
     /* START OF DEMO JS - NOT NEEDED */
     if (window.location == window.parent.location) {
         $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
-        $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
+        $('#fullscreen').attr('href', 'https://bootsnipp.com/mouse0270/snippets/PbDb5');
         $('#fullscreen').attr('title', 'Back To Bootsnipp');
     }    
     $('#fullscreen').on('click', function(event) {
