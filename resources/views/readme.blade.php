58fa588ae4cda037a9c9bd3a64967877f4afe3be
Jonathan Fritz <jonathanfritz92@gmail.com>
2019-04-30 21:32:23 -0300

    -update readme

--- resources/views/readme.blade.php
+++ resources/views/readme.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Read Me</div>
+                <div class="card-header">Acerca de</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -35,6 +35,55 @@
                                     <td>2019</td>
                                 </tr>
                             </tbody>                     
+                        </table>
+                    </div>
+                </div>
+
+                <div class="card-header">Datos Obtenidos de Audit</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <div class="table-responsive-md">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>URL</th>
+                                    <th>Performance</th>
+                                    <th>Accesibilidad</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>/home</td>
+                                    <td>87%</td>
+                                    <td>87%</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>/login</td>
+                                    <td>88%</td>
+                                    <td>89%</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>/book</td>
+                                    <td>88%</td>
+                                    <td>87%</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>/list</td>
+                                    <td>89%</td>
+                                    <td>87%</td>
+                                </tr>
+                            </tbody>                
                         </table>
                     </div>
                 </div>
