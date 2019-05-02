aff6cc3e7574c22816b3f46c55a08697571251fc
Constanza Giorgetti <coti.giorgetti@hotmail.com>
2019-05-02 19:06:08 -0300

    Middleware para todas las rutas que requieren login previo

--- routes/web.php
+++ routes/web.php
@@ -22,33 +22,33 @@ Route::get('/list/{id}',"ListController@show")->name('showlist');
 
 Route::get('/create_list',"ListController@create")->name('create_list')->middleware('auth'); ;
 
-Route::post('/create_list',"ListController@store");
+Route::post('/create_list',"ListController@store")->middelware('auth');
 
 //Seccion agregar pelis y editar pelis 
 
-Route::get('/editlist/{id}','MovieController@create')->name('editlist');
+Route::get('/editlist/{id}','MovieController@create')->name('editlist')->middleware('auth');
 
-Route::post('/editlist/{id}',"MovieController@store");
+Route::post('/editlist/{id}',"MovieController@store")->middleware('auth');
 
 //Editar - eliminar peliculas
 
-Route::delete('/editlist','MovieController@destroy')->name('delete_movie');
+Route::delete('/editlist','MovieController@destroy')->name('delete_movie')->middleware('auth');
 
-Route::post('/editlist', 'MovieController@edit')->name('edit_movie');
+Route::post('/editlist', 'MovieController@edit')->name('edit_movie')->middleware('auth');
 
 //Editar usuario
 
-Route::get('/edit','UserController@edit')->name('edituser');
+Route::get('/edit','UserController@edit')->name('edituser')->middleware('auth');
 
-Route::post('/edit','UserController@update');
+Route::post('/edit','UserController@update')->middleware('auth');
 
 //Seccion mis listas
 
-Route::get('/lists','UserController@mylists')->name('lists');
+Route::get('/lists','UserController@mylists')->name('lists')->middleware('auth');
 
-Route::delete('/lists','ListController@destroy')->name('delete_list');
+Route::delete('/lists','ListController@destroy')->name('delete_list')->middleware('auth');
 
-Route::post('/lists', 'ListController@edit')->name('edit_list');
+Route::post('/lists', 'ListController@edit')->name('edit_list')->middleware('auth');
 
 //Routes for lists of elements
 
