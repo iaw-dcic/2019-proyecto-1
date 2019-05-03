561930999542ce845b5630465cd77226fff84cdf
Matias <matimassetti@gmail.com>
2019-05-03 15:34:37 -0300

    Improving performance:  Defering CSS and removing bootstrap css. Used CDN instead

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -14,25 +14,28 @@
   <link href="img/favicon.ico" rel="shortcut icon" />
 
   <!-- Google Font -->
-  <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+  <link difer href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
 
 
   <!-- Stylesheets -->
 
-  <link rel="stylesheet" href="{{ URL::asset('/css/bootstrap.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('/css/font-awesome.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('/css/owl.carousel.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('/css/animate.css') }}">
+  <link defer rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+
+  <link defer rel="stylesheet" href="{{ URL::asset('/css/font-awesome.min.css') }}">
+  <link defer rel="stylesheet" href="{{ URL::asset('/css/owl.carousel.min.css') }}">
+  <link defer rel="stylesheet" href="{{ URL::asset('/css/animate.css') }}">
    
   <!-- Form -->
-  <link rel="stylesheet" href="{{ URL::asset('/css/style-form-game.css') }}">
+  <link defer rel="stylesheet" href="{{ URL::asset('/css/style-form-game.css') }}">
 
 	<!-- Style-CSS -->
-  <link rel="stylesheet" href="{{ URL::asset('/css/fontawesome-all.css') }}">
+  <link defer rel="stylesheet" href="{{ URL::asset('/css/fontawesome-all.css') }}">
   
 		<!-- web-fonts -->
-	<link href="//fonts.googleapis.com/css?family=Marck+Script&amp;subset=cyrillic,latin-ext" rel="stylesheet">
-	<link href="//fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext"
+	<link defer href="//fonts.googleapis.com/css?family=Marck+Script&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+	<link defer href="//fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext"
       rel="stylesheet">
       
 
@@ -40,15 +43,14 @@
   <link rel="stylesheet" href="{{ URL::asset('/css/style.css') }}">
 
   <!-- Search table -->
-  <link rel="stylesheet" href="{{ URL::asset('/css/search-table-util.css') }}">
   @yield('search-specific-style')
 
   <!--Multiple select forms Bootstrap-->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+  <link defer rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+  <link defer rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
 
   <!-- Faq -->
-  <link rel="stylesheet" href="{{ URL::asset('/css/faq-custom.css') }}"> 
+  <link defer rel="stylesheet" href="{{ URL::asset('/css/faq-custom.css') }}"> 
 
 
 </head>
