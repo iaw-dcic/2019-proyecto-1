07a11352115ec3001a5f3bb8f2c8b98c86aed3b6
gaston <reyesgaston_95@yahoo.com.ar>
2019-05-06 13:47:23 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/ListasPublicas', 'UserController@index')->name('ListasPublicas');
 Route::get('/Coleccion/{id}', 'ColeccionController@index')->name('Coleccion');
 Route::resource('Categorias','CategoriaController');
 
-Route::get('/home/{nom}/{cate}','CategoriaController@destroy')->name('home');
-Route::get('/home/{nom}','CategoriaController@edit')->name('home');
+Route::get('/home/{nom}/{cate}','CategoriaController@destroy')->name('home')->middleware('auth');
+Route::get('/home/{nom}','CategoriaController@edit')->name('home')->middleware('auth');
 Route::get('/Persona','PersonalController@index');
 
 //Route::get('/login{provider}','SocialAuthController@redirectToProvider');
