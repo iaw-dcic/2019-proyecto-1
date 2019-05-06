6a71888db25dd9897c9fec4a34c4248f043a067b
Lucas Monzón <monzon.lucas4@gmail.com>
2019-05-06 17:39:09 -0300

    Base de datos
    El archivo database.php volvió a su estado original para configurarla desde el archivo .env y las variables de entorno de heroku

--- config/database.php
+++ config/database.php
@@ -44,11 +44,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'us-cdbr-gcp-east-01.cleardb.net'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'gcp_43b06802eb66400c46a5'),
-            'username' => env('DB_USERNAME', 'b0e2dcaf394811'),
-            'password' => env('DB_PASSWORD', 'cd4d01eb'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
