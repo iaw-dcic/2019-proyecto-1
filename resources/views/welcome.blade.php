6f9dee43120cb2fe2c1ab89dc2bbc75cef78e865
MicaelaWaigel <micaawaigel@gmail.com>
2019-05-07 13:32:54 -0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,9 @@
         <!-- Styles -->
         
         <link rel="stylesheet" href="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css') }}">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
-        <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet'>
+        <script src="{{ asset('https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js') }}"></script>
+        <script src="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js') }}"></script> 
+        <link href="{{ asset('https://fonts.googleapis.com/css?family=Bangers') }}" rel='stylesheet'>
         <link rel="stylesheet" href="{{ asset('css/miestilo.css') }}">
 
     </head>
