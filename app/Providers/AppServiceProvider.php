e263af5d5052676cd48031f849d4a6b1e7cef9af
ajavieralejandro <amorosijavier@gmail.com>
2019-05-07 16:04:39 -0300

    Fixed bugs.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (env('APP_ENV') === 'production') {
+            $this->app['url']->forceScheme('https');
+        }
+
     }
 
     /**
