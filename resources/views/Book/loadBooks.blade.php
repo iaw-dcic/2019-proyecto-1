c47bc27c26d168402869aa425606194e57bf58a2
Manuela <manuela.dv.fernandez@gmail.com>
2019-05-08 16:47:29 -0300

    cambios3

--- resources/views/Book/loadBooks.blade.php
+++ resources/views/Book/loadBooks.blade.php
@@ -8,37 +8,34 @@
     <p class="lead">IAW - 2019</p>
   </div>
 </div>
-<div class="container">
-  <div class="row">
 
-    <div class="col-4">
-      <form method="POST" action="/home/loadBooks/{{$id}}">
-        {{csrf_field()}}
+<div class="album py-5">
+  <form method="POST" action="/home/loadBooks/{{$id}}">
+    {{csrf_field()}}
 
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="col mb-4">
-                <div class="card">
-                  <div class="card-body">
-                    <h4>Cargar nuevo archivo</h4>
-                    <h5 class="card-subtitle mb-2 text-muted"><input type="text" class="form-control" name="title" placeholder="Titulo" required></h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><input type="text" class="form-control" name="author" placeholder="Autor" required></h6>
-                    <h6 class="card-subtitle mb-2 text-muted"><input type="text" class="form-control" name="editorial" placeholder="Editorial" required></h6>
-                    <h6 class="card-subtitle mb-2 text-muted"><input type="number" class="form-control" name="pag" placeholder="Cantidad paginas" required></h6>
-                    <button type="submit" class="btn btn-link"> Cargar</button>
-                  </div>
-                </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="col mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h4>Cargar nuevo archivo</h4>
+                <h5 class="card-subtitle mb-2 text-muted"><input type="text" class="form-control" name="title" placeholder="Titulo" required></h5>
+                <h6 class="card-subtitle mb-2 text-muted"><input type="text" class="form-control" name="author" placeholder="Autor" required></h6>
+                <h6 class="card-subtitle mb-2 text-muted"><input type="text" class="form-control" name="editorial" placeholder="Editorial" required></h6>
+                <h6 class="card-subtitle mb-2 text-muted"><input type="number" class="form-control" name="pag" placeholder="Cantidad paginas" required></h6>
+                <button type="submit" class="btn btn-link"> Cargar</button>
               </div>
             </div>
-
           </div>
         </div>
-      </form>
+
+      </div>
     </div>
+  </form>
 
-    <div class="col-8">
-
+  <div class="container">
+    <div class="row">
       @foreach ($books as $bk)
       <div class="col-md-4">
         <div class="col mb-4">
@@ -60,10 +57,10 @@
         </div>
       </div>
       @endforeach
-
     </div>
   </div>
 </div>
+
 
 
 @endsection
