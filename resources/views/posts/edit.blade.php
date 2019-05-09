746799b0c0c75ebc255cfe043ee0843a82180d97
agus-str <agus.stragua@gmail.com>
2019-05-09 16:51:09 -0300

    Cambios en edit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -36,7 +36,7 @@
                         <td>{{$item->link}}</td>
                         <td>{{$item->price}}</td>
                         <td>
-                         {{ Form::open(['action' => ['ItemsController@edit', $item->id] , 'method'=> 'GET']) }}
+                         {{ Form::open(['action' => ['ItemsController@edit', $item->id] , 'method'=> 'POST']) }}
                             {{Form::submit('Edit', ['class'=>'btn'])}}
                         {!!Form::close()!!}
       
