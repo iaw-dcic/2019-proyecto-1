02ecbd3a648bae1c4e4d79f4d0732461920c7042
RamiroFraysse <ramirofraysse@gmail.com>
2019-05-09 15:58:05 -0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Futboleros') }}</title>
 
 
     <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
