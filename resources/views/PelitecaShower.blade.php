0da466a1dbcb154b83a535c8f186390293cb0dc6
joaco1916 <joaquin_a_rodriguez@hotmail.com>
2019-06-10 01:25:05 -0300

    Cambios menores
    Cambios menores en el navbar

--- resources/views/PelitecaShower.blade.php
+++ resources/views/PelitecaShower.blade.php
@@ -8,50 +8,44 @@
 <link href="{{ asset('css/estilo.css') }}" rel="stylesheet">
 
 <form class="form-inline"  method="POST" action="{{ url('/PelitecaEditor/Generos') }}" >
-  @csrf
- @foreach ($caters as $cater)
-<div class="accordion" id="accordionExample" text-align="center" style="width:100%;">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-block btn-primary collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          {{$cater->genero}}
-        </button>
-      </h2>
-    </div>
+@csrf
+  @foreach ($caters as $cater)
+    <div class="accordion" id="accordionExample" text-align="center" style="width:100%;">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h2 class="mb-0">
+            <button class="btn btn-block btn-primary collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              {{$cater->genero}}
+            </button>
+          </h2>
+        </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        
-        @foreach ($peliculas as $pelicula)
-        @if($pelicula->genero==$cater->genero)
-       
-       <table class="table table-borderless">
-        <thead>
-          <tr> 
-            <th scope="col">Pelicula</th>
-            <th scope="col">Año</th>
-            <th scope="col">Puntaje</th> 
-          </tr>
-        </thead>
-   <tbody>
-     
-    <tr>
-
-      <td>{{$pelicula->pelicula}}</td>
-      <td>{{$pelicula->anio}} </td>
-      <td>{{$pelicula->puntaje}} </td>
-    </tr>
-  </tbody>
-</table>
-            
-
-           
-       @endif
-      @endforeach
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+          <div class="card-body">
+            @foreach ($peliculas as $pelicula)
+              @if($pelicula->genero==$cater->genero)
+                <table class="table table-borderless">
+                  <thead>
+                    <tr> 
+                      <th scope="col">Pelicula</th>
+                      <th scope="col">Año</th>
+                      <th scope="col">Puntaje</th> 
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{{$pelicula->pelicula}}</td>
+                      <td>{{$pelicula->anio}} </td>
+                      <td>{{$pelicula->puntaje}} </td>
+                    </tr>
+                  </tbody>
+                </table>
+              @endif
+            @endforeach
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    @endif
   @endforeach
   
 @endsection
