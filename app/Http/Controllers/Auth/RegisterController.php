d45310de523cbec45a063d1f5b6fc91a474b517d
Ramiro Fraysse <ramirofraysse@gmail.com>
2020-07-29 03:18:32 -0300

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'activar2F'=> 0,
+            'semilla' => '0',
         ]);
     }
 }
